a15ab0f2e487c3207f7245e5186d27565d29869c
Ryan W <42725021+IsThatLogic@users.noreply.github.com>
2022-02-21 14:38:23 -0500

    UI, react-scripts -> v5.0.0, Changed 404

--- src/Components/404.jsx
+++ src/Components/404.jsx
@@ -6,27 +6,16 @@ export default HTTP404;
 function HTTP404() {
 	return(
 		<>	
-		<center>
+		<div className="fourofour">
 
-		{window.onload = function(ev) {
-			for(let i=0; i<20; ++i)
-			{
-				let div = document.createElement('div');
-				div.innerHTML = "404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found ";
-				div.classList.add("main-content");
-				document.body.appendChild(div);
-			}
-		}
-		}
-		{window.onscroll = function(ev) {
-    	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-       			let div = document.createElement('div');
-				   div.innerHTML = "404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found 404 Not Found ";
-				div.classList.add("main-content");
-				document.body.appendChild(div);
-    		}
-		}}
-		</center>
+			<div className="container">
+		
+					Whoops!
+ <br/>
+				Looks like you're trying to find a page that <b>doesn't exist</b>.<br/><br/>
+				Perhaps you are looking for a page that has been moved?<br/>
+			</div>
+		</div>
 		</>
 	)	
 }
